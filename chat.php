ed12e6029105a7a5286c3e5a0fabe2411172f970
Eramik <erami.ukr@gmail.com>
2016-03-21 20:15:42 +0200

    small fixes

--- chat.php
+++ chat.php
@@ -6,7 +6,7 @@ error_reporting(E_ALL);
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
 		if($_POST["mode"] == "Get history"){
-			echo file_get_contents("log.json") ; //!
+			echo file_get_contents("log.json");
 		}
 		else if($_POST["mode"] == "New message") {
 			$log = json_decode(file_get_contents("log.json"), true);
